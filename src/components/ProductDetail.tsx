46655550d0532738085165ebfa1f408a8c921853
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-02-22 12:37:18 +0800

    style: product detail card

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -47,44 +47,47 @@ export default function ProductDetail({
   const postDate = dayjs(created_at).format("YYYY-MM-DD");
 
   return (
-    <div>
+    <>
       <Breadcrumb items={breadcrumbItems} className="mb-4" />
+      <div className="flex flex-col items-center">
 
-      {/* product card */}
-      <div className="bg-white flex flex-col items-center">
-        <Image
-          src={image_url}
-          alt={`image of ${product_name}`}
-          width={300}
-          height={300}
-          className="mt-4"
-        />
-        <p className="text-lg font-bold p-4">{product_name}</p>
-      </div>
+        {/* product card */}
+        <div className="flex flex-col items-center w-[332px] bg-white">
+          <Image
+            src={image_url}
+            alt={`image of ${product_name}`}
+            width={300}
+            height={300}
+            className="mt-4 mx-4"
+          />
+          <p className="bg-white w-[300px] text-lg font-bold p-4">{product_name}</p>
+        </div>
 
-      {/* product detail section */}
-      <div className="space-y-2 my-2">
-        <div className="flex flex-row gap-2">
-          <p className="w-[100px] flex-shrink-0">Price:</p>
-          <div>{price}</div>
-        </div>
-        <div className="flex flex-row gap-2">
-          <p className="w-[100px] flex-shrink-0">Quantity:</p>
-          <div>{quantity}</div>
-        </div>
-        <div className="flex flex-row gap-2">
-          <p className="w-[100px] flex-shrink-0">Description:</p>
-          <div>{description}</div>
-        </div>
-        <div className="flex flex-row gap-2">
-          <p className="w-[100px] flex-shrink-0">Artisan:</p>
-          <div className="underline"><Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/artisans/${artisan_id}`}>{first_name} {last_name}</Link></div>
-        </div>
-        <div className="flex flex-row gap-2">
-          <p className="w-[100px] flex-shrink-0">Post Date:</p>
-          <div>{postDate}</div>
+        {/* product detail section */}
+        <div className="space-y-2 my-2 max-w-[550px]">
+          <div className="flex flex-row gap-2">
+            <p className="w-[100px] flex-shrink-0">Price:</p>
+            <div>{price}</div>
+          </div>
+          <div className="flex flex-row gap-2">
+            <p className="w-[100px] flex-shrink-0">Quantity:</p>
+            <div>{quantity}</div>
+          </div>
+          <div className="flex flex-row gap-2">
+            <p className="w-[100px] flex-shrink-0">Description:</p>
+            <div>{description}</div>
+          </div>
+          <div className="flex flex-row gap-2">
+            <p className="w-[100px] flex-shrink-0">Artisan:</p>
+            <div className="underline"><Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/artisans/${artisan_id}`}>{first_name} {last_name}</Link></div>
+          </div>
+          <div className="flex flex-row gap-2">
+            <p className="w-[100px] flex-shrink-0">Post Date:</p>
+            <div>{postDate}</div>
+          </div>
         </div>
       </div>
-    </div>
+    
+    </>
   );
 }
