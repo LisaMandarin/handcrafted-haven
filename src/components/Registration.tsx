96bdffe76e73cba71de68b61533de306b414bb47
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-02-24 01:55:12 +0800

    fix: toggle show password

--- src/components/Registration.tsx
+++ src/components/Registration.tsx
@@ -1,7 +1,12 @@
-'use client'
+"use client";
 import React, { useState } from "react";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
+import { IoEyeOutline } from "react-icons/io5";
 
 export default function Registration() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
   const [formData, setFormData] = useState({
     username: "",
     email: "",
@@ -13,7 +18,7 @@ export default function Registration() {
     email: "",
     password: "",
     confirmPassword: "",
-  })
+  });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -21,88 +26,89 @@ export default function Registration() {
   };
 
   const validateField = (name: string, value: string) => {
-    let errorMsg = ""
+    let errorMsg = "";
 
     if (name === "username") {
-        if (!value) {
-            errorMsg = "Username is required"
-        } else if (value.length < 2) {
-            errorMsg = "Username must be more than 1 character"
-        }
+      if (!value) {
+        errorMsg = "Username is required";
+      } else if (value.length < 2) {
+        errorMsg = "Username must be more than 1 character";
+      }
+    } else if (name === "email") {
+      if (!value) {
+        errorMsg = "Email is required";
+      } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
+        errorMsg = "Invalid email format";
+      }
+    } else if (name === "password") {
+      if (!value) {
+        errorMsg = "Password is required";
+      } else if (value.length < 8) {
+        errorMsg = "Password must be at least 8 characters long";
+      } else if (!/(?=.*[a-zA-Z])(?=.*\d)/.test(value)) {
+        errorMsg = "At least one letter and one number";
+      }
+    } else if (name === "confirmPassword") {
+      if (!value) {
+        errorMsg = "Please repeat password";
+      } else if (value !== formData.password) {
+        errorMsg = "Password doesn't match";
+      }
     }
-    else if (name === "email") {
-        if (!value) {
-            errorMsg = "Email is required"
-        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value)) {
-            errorMsg = "Invalid email format"
-        }
-    }
-    else if (name === "password") {
-        if (!value) {
-            errorMsg = "Password is required"
-        } else if (value.length < 8) {
-            errorMsg = "Password must be at least 8 characters long"
-        } else if (!/(?=.*[a-zA-Z])(?=.*\d)/.test(value)) {
-            errorMsg = "At least one letter and one number"
-        }
-    }
-    else if (name === "confirmPassword") {
-        if (!value) {
-            errorMsg = "Please repeat password"
-        } else if (value !== formData.password) {
-            errorMsg = "Password doesn't match"
-        }
-    }
-    return errorMsg
-  }
+    return errorMsg;
+  };
 
   const onBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    const {name, value} = e.target
-    setErrors({...errors, [name]: validateField(name, value)})
-  } 
-  const handleSubmit = async(e: React.FormEvent) => {
+    const { name, value } = e.target;
+    setErrors({ ...errors, [name]: validateField(name, value) });
+  };
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
     const newErrors = {
-        username: validateField('username', formData.username),
-        email: validateField('email', formData.email),
-        password: validateField('password', formData.password),
-        confirmPassword: validateField('confirmPassword', formData.confirmPassword)
-    }
-    setErrors(newErrors)
-    
+      username: validateField("username", formData.username),
+      email: validateField("email", formData.email),
+      password: validateField("password", formData.password),
+      confirmPassword: validateField(
+        "confirmPassword",
+        formData.confirmPassword
+      ),
+    };
+    setErrors(newErrors);
+
     const hasError = (value: string) => {
-        if (value) {
-            return true
-        }
-    } 
+      if (value) {
+        return true;
+      }
+    };
 
     if (Object.values(errors).some(hasError)) {
-        alert('Form has errors')
-        return
+      alert("Form has errors");
+      return;
     }
-    
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/register`, {
-            method: "POST",
-            headers: {
-                "Content-Type" : "application/json"
-            },
-            body: JSON.stringify(formData)
-        });
-        const result = await response.json();
-        
-        if (result.success) {
-            alert(`${result.message}`)
-        } else {
-            alert(`${result.message}`)
-        }
-    } catch (error) {
-        console.error("Error during registration: ", error)
-        alert('An error occurred during registration')
-    }
-    
 
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/register`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+      const result = await response.json();
+
+      if (result.success) {
+        alert(`${result.message}`);
+      } else {
+        alert(`${result.message}`);
+      }
+    } catch (error) {
+      console.error("Error during registration: ", error);
+      alert("An error occurred during registration");
+    }
   };
   return (
     <div className="max-w-[370px] w-full">
@@ -120,7 +126,11 @@ export default function Registration() {
             placeholder="Enter your username"
             className="w-full text-lg py-2 px-2 rounded-xl border border-b-custom-yellow-2 border-b-4 focus:border-custom-brown-1 focus:outline-none"
           />
-          {errors.username && <p className="text-sm text-red-600 px-2 absolute left-0">{errors.username}</p>}
+          {errors.username && (
+            <p className="text-sm text-red-600 px-2 absolute left-0">
+              {errors.username}
+            </p>
+          )}
         </div>
         <div className="relative">
           <input
@@ -132,23 +142,37 @@ export default function Registration() {
             placeholder="Enter your email"
             className="w-full text-lg py-2 px-2 rounded-xl border border-b-custom-yellow-2 border-b-4 focus:border-custom-brown-1 focus:outline-none"
           />
-          {errors.email && <p className="text-sm text-red-600 px-2 absolute left-0">{errors.email}</p>}
+          {errors.email && (
+            <p className="text-sm text-red-600 px-2 absolute left-0">
+              {errors.email}
+            </p>
+          )}
         </div>
         <div className="relative">
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             value={formData.password}
             onChange={onChange}
             onBlur={onBlur}
             placeholder="Create password"
-            className="w-full text-lg py-2 px-2 rounded-xl border border-b-custom-yellow-2 border-b-4 focus:border-custom-brown-1 focus:outline-none"
+            className="relative w-full text-lg py-2 px-2 rounded-xl border border-b-custom-yellow-2 border-b-4 focus:border-custom-brown-1 focus:outline-none"
           />
-          {errors.password && <p className="text-sm text-red-600 px-2 absolute left-0">{errors.password}</p>}
+          <span
+            className="absolute right-2 top-1/2 -translate-y-1/2  text-xl"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <AiOutlineEyeInvisible /> : <IoEyeOutline />}
+          </span>
+          {errors.password && (
+            <p className="text-sm text-red-600 px-2 absolute left-0">
+              {errors.password}
+            </p>
+          )}
         </div>
         <div className="relative">
           <input
-            type="password"
+            type={showConfirmPassword ? "text" : "password"}
             name="confirmPassword"
             value={formData.confirmPassword}
             onChange={onChange}
@@ -156,10 +180,21 @@ export default function Registration() {
             placeholder="Confirm password"
             className="w-full text-lg py-2 px-2 rounded-xl border border-b-custom-yellow-2 border-b-4 focus:border-custom-brown-1 focus:outline-none"
           />
-          {errors.confirmPassword && <p className="text-sm text-red-600 px-2 absolute left-0">{errors.confirmPassword}</p>}
+          <span
+            className="absolute right-2 top-1/2 -translate-y-1/2  text-xl"
+            onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+          >
+            {showConfirmPassword ? <AiOutlineEyeInvisible /> : <IoEyeOutline />}
+          </span>
+          
+          {errors.confirmPassword && (
+            <p className="text-sm text-red-600 px-2 absolute left-0">
+              {errors.confirmPassword}
+            </p>
+          )}
         </div>
         <div>
-            <input type="hidden" name="created_at"/>
+          <input type="hidden" name="created_at" />
         </div>
         <div>
           <input
