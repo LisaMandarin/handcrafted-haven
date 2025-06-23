2f55af0c64eae4acb39bde12279142f99203f539
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-06-23 23:01:10 +0800

    fix: add icons to buttons

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,5 +1,7 @@
 'use client'
-
+import { EditOutlined } from "@ant-design/icons";
+import { AiOutlineCheck } from "react-icons/ai";
+import { AiOutlineClose } from "react-icons/ai";
 import LoginButton from "@/components/LoginButton";
 import { Divider, Spin } from "antd";
 import dayjs from "dayjs";
@@ -189,13 +191,22 @@ export default function ProfilePage() {
           <div className="flex justify-center">
             {!editMode && (
               <div className="flex flex-row p-4">
-                <button className="px-3 py-1 lg:px-6 h-fit bg-custom-dark-brown text-custom-yellow-1 md:rounded-3xl lg:rounded-full" onClick={() => setEditMode(true)}>Edit</button>
+                <button className="px-3 py-1 lg:px-6 h-fit bg-custom-dark-brown text-custom-yellow-1 md:rounded-3xl lg:rounded-full" onClick={() => setEditMode(true)}>
+                  <EditOutlined />
+                  Edit
+                </button>
               </div>
             )}
             {editMode && (
               <div className="flex flex-row gap-2 p-4">
-                <button className="px-3 py-1 lg:px-6 h-fit bg-custom-dark-brown text-custom-yellow-1 md:rounded-3xl lg:rounded-full" onClick={() => handleUpdate()}>Update</button>
-                <button className="px-3 py-1 lg:px-6 h-fit bg-custom-yellow-1 text-custom-dark-brown border border-custom-dark-brown md:rounded-3xl lg:rounded-full" onClick={() => handleCancel()}>Cancel</button>
+                <button className="px-3 py-1 lg:px-6 h-fit bg-custom-dark-brown text-custom-yellow-1 md:rounded-3xl lg:rounded-full" onClick={() => handleUpdate()}>
+                  <AiOutlineCheck className="inline"/>
+                  Update
+                </button>
+                <button className="px-3 py-1 lg:px-6 h-fit bg-custom-yellow-1 text-custom-dark-brown border border-custom-dark-brown md:rounded-3xl lg:rounded-full" onClick={() => handleCancel()}>
+                  <AiOutlineClose className="inline" />
+                  Cancel
+                </button>
               </div>
             )}
           </div>
