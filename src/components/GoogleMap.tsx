16392225e3d761fb00ddfd6bb901640e933116ca
Lisa <122068169+LisaMandarin@users.noreply.github.com>
2025-06-24 15:38:59 +0800

    fix: google map key

--- src/components/GoogleMap.tsx
+++ src/components/GoogleMap.tsx
@@ -1,6 +1,7 @@
 export default function GoogleMap({address}: {address: string}) {
     const encodedAddress= encodeURIComponent(address);
     const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
+    console.log('apiKey: ', apiKey);
     
     return (
         <div className="flex justify-center">
